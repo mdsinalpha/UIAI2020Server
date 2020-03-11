113b62349f46c7d982cf69653a4e518c5bcdef40
mdsinalpha <mohsendb7008@gmail.com>
2020-03-11 13:55:05 +0330

    StreetSpace class added.

--- models/Board.kt
+++ models/Board.kt
@@ -3,3 +3,6 @@ package models
 data class Position(val column: Int, val step: Int)
 
 sealed class Cell(val position: Position, val adjacents : List<Cell>)
+
+class StreetSpace(position: Position, adjacents: List<Cell>,
+                  val strictTile: Boolean = false): Cell(position, adjacents)

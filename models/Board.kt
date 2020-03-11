756852ce86188a6c7a0744b9313e73cb1424b56d
ghazal <ghazal.rb1998@gmail.com>
2020-03-11 19:05:31 +0330

    Game class added.

--- models/Board.kt
+++ models/Board.kt
@@ -1,5 +1,7 @@
 package models
 
+import kotlin.random.Random
+
 data class Position(val column: Int, val step: Int)
 
 sealed class Cell(val position: Position, val adjacents : List<Cell>, var character: Character? = null)
@@ -43,4 +45,28 @@ class InvestigationTile private constructor(val blockedCells: Pair<Cell, Cell>) 
         val InvestigationTile1: InvestigationTile = TODO()
         val InvestigationTile2: InvestigationTile = TODO()
     }
+}
+
+object cardDealer{
+    private var previousCards: MutableList<Character> = ArrayList<Character>()
+
+    fun deal(turn: Int): MutableList<Character> {
+        val leftCards = Character.values().toMutableList()
+        return if(turn % 2 == 0) {
+            val tempArray = ArrayList<Character>()
+            for(i in 1..4)
+                tempArray.add(leftCards.removeAt(Random(leftCards.size).nextInt()))
+            previousCards = tempArray
+            leftCards
+        } else leftCards
+    }
+}
+
+class Game(private var round: Int = 1, private var turn: Int = 1, private var jackVisibility: Boolean = true) {
+    private var cards: MutableList<Character> = cardDealer.deal(turn)
+    private var visibleCharacters: List<Character> = TODO()
+    private var invisibleCharacters: List<Character> = TODO()
+    private var innocentCharacters: List<Character> = TODO()
+
+    fun run(): Unit = TODO()
 }

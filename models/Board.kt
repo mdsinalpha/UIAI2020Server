98f5078c2a1480ac85178fb808731de11f45c932
mdsinalpha <mohsendb7008@gmail.com>
2020-03-11 14:51:52 +0330

    QuaysideSpace class added.

--- models/Board.kt
+++ models/Board.kt
@@ -6,3 +6,13 @@ sealed class Cell(val position: Position, val adjacents : List<Cell>)
 
 class StreetSpace(position: Position, adjacents: List<Cell>,
                   val strictTile: Boolean = false): Cell(position, adjacents)
+
+class QuaysideSpace private constructor(position: Position,
+                                        adjacents: List<Cell>): Cell(position, adjacents){
+
+    companion object{
+        val QuaysideSpace1: QuaysideSpace = TODO()
+        val QuaysideSpace2: QuaysideSpace = TODO()
+        val QuaysideSpace3: QuaysideSpace = TODO()
+    }
+}

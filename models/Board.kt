c413bcf2bc5e5d2e9d49f03cb4d580f3a9b460a9
mdsinalpha <mohsendb7008@gmail.com>
2020-03-11 15:47:08 +0330

    Character field added to Cell class.

--- models/Board.kt
+++ models/Board.kt
@@ -2,7 +2,7 @@ package models
 
 data class Position(val column: Int, val step: Int)
 
-sealed class Cell(val position: Position, val adjacents : List<Cell>)
+sealed class Cell(val position: Position, val adjacents : List<Cell>, var character: Character? = null)
 
 class StreetSpace(position: Position, adjacents: List<Cell>,
                   val strictTile: Boolean = false): Cell(position, adjacents)

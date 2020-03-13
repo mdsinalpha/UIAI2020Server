eee29b89aa874f5a26b88f629a61e1310c46642b
ghazal <ghazal.rb1998@gmail.com>
2020-03-14 01:40:14 +0330

    initial tiles' position added.

--- models/Board.kt
+++ models/Board.kt
@@ -15,10 +15,12 @@ class StreetSpace private constructor(position: Position, vararg adjacents: Posi
         val map = listOf(
             StreetSpace(1 pos 10, 1 pos 8, 2 pos 9, 2 pos 11),
             StreetSpace(2 pos 7, 1 pos 8, 2 pos 9, 3 pos 6, 3 pos 8),
-            StreetSpace(2 pos 9, 1 pos 8, 1 pos 10, 2 pos 7, 2 pos 11, 3 pos 8, 3 pos 10),
+            StreetSpace(2 pos 9, 1 pos 8, 1 pos 10, 2 pos 7, 2 pos 11, 3 pos 8, 3 pos 10,
+                tile = Tile.MetroEntrance),
             StreetSpace(2 pos 11, 1 pos 10, 2 pos 9, 2 pos 13, 3 pos 10, 3 pos 12),
             StreetSpace(3 pos 8, 2 pos 7, 2 pos 9, 3 pos 6, 3 pos 10, 4 pos 7, 4 pos 9),
-            StreetSpace(3 pos 10, 2 pos 9, 2 pos 11, 3 pos 8, 3 pos 12, 4 pos 9, 4 pos 11),
+            StreetSpace(3 pos 10, 2 pos 9, 2 pos 11, 3 pos 8, 3 pos 12, 4 pos 9, 4 pos 11,
+                tile = Tile.GasLamp),
             StreetSpace(3 pos 12, 2 pos 11, 2 pos 13, 3 pos 10, 3 pos 14, 4 pos 11, 4 pos 13),
             StreetSpace(3 pos 14, 2 pos 13, 3 pos 12, 4 pos 13, 4 pos 15),
             StreetSpace(4 pos 5, 3 pos 6, 4 pos 7, 5 pos 4, 5 pos 6),
@@ -70,7 +72,8 @@ class StreetSpace private constructor(position: Position, vararg adjacents: Posi
                 tile = Tile.BuildingSite, strictTile = true),
             StreetSpace(10 pos 13, 9 pos 12, 10 pos 11, 11 pos 12, 11 pos 14),
             StreetSpace(11 pos 2, 10 pos 3, 11 pos 4, 12 pos 1, 12 pos 3),
-            StreetSpace(11 pos 4, 10 pos 3, 10 pos 5, 11 pos 2, 11 pos 6, 12 pos 3, 12 pos 5),
+            StreetSpace(11 pos 4, 10 pos 3, 10 pos 5, 11 pos 2, 11 pos 6, 12 pos 3, 12 pos 5,
+                tile = Tile.MetroEntrance),
             StreetSpace(11 pos 6, 10 pos 5, 10 pos 7, 11 pos 4, 11 pos 8, 12 pos 5, 12 pos 7,
                 tile = Tile.BuildingSite, strictTile = true),
             StreetSpace(11 pos 8, 10 pos 7, 10 pos 9, 11 pos 6, 11 pos 10, 12 pos 7, 12 pos 9),
@@ -83,14 +86,16 @@ class StreetSpace private constructor(position: Position, vararg adjacents: Posi
             StreetSpace(12 pos 7, 11 pos 6, 11 pos 8, 12 pos 5, 12 pos 9, 13 pos 6, 13 pos 8),
             StreetSpace(12 pos 9, 11 pos 8, 11 pos 10, 12 pos 7, 12 pos 11, 13 pos 8, 13 pos 10),
             StreetSpace(12 pos 11, 11 pos 10, 11 pos 12, 12 pos 9, 12 pos 13, 13 pos 10, 13 pos 12),
-            StreetSpace(12 pos 13, 11 pos 12, 11 pos 14, 12 pos 11, 12 pos 15, 13 pos 12, 13 pos 14),
+            StreetSpace(12 pos 13, 11 pos 12, 11 pos 14, 12 pos 11, 12 pos 15, 13 pos 12, 13 pos 14,
+                tile = Tile.MetroEntrance),
             StreetSpace(12 pos 15, 11 pos 14, 11 pos 16, 12 pos 13, 13 pos 14, 13 pos 16),
             StreetSpace(13 pos 2, 12 pos 1, 12 pos 3, 13 pos 4),
             StreetSpace(13 pos 4, 12 pos 3, 12 pos 5, 13 pos 2, 13 pos 6),
             StreetSpace(13 pos 6, 12 pos 5, 12 pos 7, 13 pos 4, 13 pos 8),
             StreetSpace(13 pos 8, 12 pos 7, 12 pos 9, 13 pos 6, 13 pos 10),
             StreetSpace(13 pos 10, 12 pos 9, 12 pos 11, 13 pos 8, 13 pos 12, 14 pos 11),
-            StreetSpace(13 pos 12, 12 pos 11, 12 pos 13, 13 pos 10, 13 pos 14, 14 pos 11),
+            StreetSpace(13 pos 12, 12 pos 11, 12 pos 13, 13 pos 10, 13 pos 14, 14 pos 11,
+                tile = Tile.GasLamp),
             StreetSpace(13 pos 14, 12 pos 13, 12 pos 15, 13 pos 12, 13 pos 16),
             StreetSpace(13 pos 16, 12 pos 15, 13 pos 14)
         ).map { it.position to it }.toMap()
@@ -133,15 +138,23 @@ object LandExit: Cell(14 pos 11)
 enum class Tile(val cells: MutableList<StreetSpace>) {
 
     BuildingSite(mutableListOf(
-
+        StreetSpace.map[4 pos 9]!!,
+        StreetSpace.map[4 pos 11]!!,
+        StreetSpace.map[10 pos 5]!!,
+        StreetSpace.map[10 pos 11]!!,
+        StreetSpace.map[11 pos 6]!!,
+        StreetSpace.map[11 pos 10]!!
     )),
 
     MetroEntrance(mutableListOf(
-
+        StreetSpace.map[2 pos 9]!!,
+        StreetSpace.map[11 pos 4]!!,
+        StreetSpace.map[12 pos 13]!!
     )),
 
     GasLamp(mutableListOf(
-
+        StreetSpace.map[3 pos 10]!!,
+        StreetSpace.map[13 pos 12]!!
     )),
 
     Park(ArrayList())
